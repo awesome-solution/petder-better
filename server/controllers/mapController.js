c4a551b4eb0f94ba8461ff65272179ec79ba6e82
Daniel Mahoney <daniel.mahoney55@gmail.com>
2024-06-17 14:50:59 -0400

    searchNearby needs headers

--- server/controllers/mapController.js
+++ server/controllers/mapController.js
@@ -28,14 +28,27 @@ mapController.searchPlaces = async (req, res, next) => {
   try {
     //const JKWT = placesClient.auth.fromAPIKey(process.env.MAPS_API);
     //const creds = await placesClient.in
-    const text = '';
-
-    const result = await placesClient.autocompletePlaces({
-      input: text,
-      includedPrimaryTypes: ['dog_park'],
-      origin: { lat: lat, lng: lon },
-      sessionToken: uuid(),
+    //const text = '';
+    const result = await placesClient.searchNearby({
+      includedTypes: ['dog_park'],
+      locationRestriction: {
+        circle: {
+          center: {
+            latitude: lat,
+            longitude: lon,
+          },
+          radius: 500.0,
+        },
+      }, //{ lat: lat, lng: lon },
     });
+
+    // const result = await placesClient.autocompletePlaces({
+    //   input: text,
+
+    //   // includedPrimaryTypes: ['dog_park'],
+    //   origin: { lat: lat, lng: lon },
+    //   sessionToken: uuid(),
+    // });
 
     res.locals.result = result;
     next();
